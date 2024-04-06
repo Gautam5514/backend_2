3bc367e04dcbfdf616030acb0e430b33813c09b8
Gautam pandit <gautampandit797@gmail.com>
2024-04-07 00:55:18 +0530

    This is the new some added

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/facebook", (req, res) => {
+    res.send("Gautam Einstein_Baba");
 })
 
-app.get('/facebook', (req, res) => {
-    res.send("Hey Man this is the man, The Myth, The Gautam");
+app.get("/collage", (req, res) => {
+    res.send("<h1>This is the collage time</h1>")
+})
+
+app.get("/code", (req,res) => {
+    res.send("<h2>This is the coding time</h2>")
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
